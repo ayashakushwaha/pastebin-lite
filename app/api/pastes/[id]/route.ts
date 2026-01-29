4b3315aed800f65eed269a0e3c3b3b1bd9bbdb58
ayasha kushwaha <ayashakushwaha2@gmail.com>
2026-01-29 14:23:14 +0530

    build fix

--- app/api/pastes/[id]/route.ts
+++ app/api/pastes/[id]/route.ts
@@ -1,28 +1,19 @@
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
-  request: Request,
-  { params }: { params: { id: string } }
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const { id } = params
-
-  const paste = ["1"].find(p => p)
-
-  if (!paste) {
-    return NextResponse.json(
-      { error: "Paste not found" },
-      { status: 404 }
-    )
-  }
+ 
+  const { id } = await params;
 
   return NextResponse.json(
     {
-        data: {
-            "content": "string",
-            "remaining_views": 4,
-            "expires_at": "2026-01-01T00:00:00.000Z"
-        }
+      content: "string",
+      remaining_views: 4,
+      expires_at: "2026-01-01T00:00:00.000Z",
+      id,
     },
-    { status : 200 }
-  )
+    { status: 200 }
+  );
 }
